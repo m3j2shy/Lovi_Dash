651ee3f3292d4fcb2994c22038ef945ea3651f1c
이진서/연구원/ID광고/컨텐츠솔루션Project <jinseo.lee@lge.com>
2025-04-25 09:18:48 +0900

    트래픽 상세 페이지

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,9 +5,22 @@ from .utils import (
     create_404_page
 )
 
+from .traffic_utils import (
+    get_traffic_per_day,
+    get_traffic_per_hour,
+    fig_traffic_per_hour,
+    get_traffic_avg_per_hour,
+)
+
 __all__ = [
     'get_bigquery_config',
     'load_bigquery_data',
     'get_sample_data',
-    'create_404_page'
+    'create_404_page',
+    'get_traffic_per_day',
+    'get_traffic_per_hour',
+    'get_traffic_avg_per_hour',
+    'get_unique_users_per_day'
+    'fig_traffic_per_day',
+    'fig_traffic_per_hour'
 ] 
